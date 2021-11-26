733303e5b454ab67e355c7349820aa6f6b6340f0
Julia Prisby <jumapr@umich.edu>
2021-11-26 15:51:35 -0500

    Add form.html to allow user to enter search parameters in an HTML form. Modify index.html to link to form.html. Modify app.py to handle search from HTML form.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import inspect
-
 import refactor
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, abort
 from database import Database
 
 app = Flask(__name__)
@@ -10,6 +9,10 @@ db = Database()
 # error messages for JSON responses
 errors = {'contestants': {"Not Found:": "Sorry, we couldn't find any contestants matching that criteria."},
           'episodes': {"Not Found:": "Sorry, we couldn't find any episodes matching that criteria."}}
+
+search_funcs = {'contestants': db.search_contestants, 'episodes': db.search_episodes}
+search_params = {'contestants': tuple(inspect.getfullargspec(search_funcs['contestants'])[0][1:]),
+                 'episodes': tuple(inspect.getfullargspec(search_funcs['episodes'])[0][1:])}
 
 
 @app.route("/")
@@ -27,11 +30,31 @@ def get_all(table: str):
 
 
 @app.route('/search/<table>')
-def search(table: str):
-    search_funcs = {'contestants': db.search_contestants, 'episodes': db.search_episodes}
-    search_params = {'contestants': tuple(inspect.getfullargspec(search_funcs['contestants'])[0][1:]),
-                     'episodes': tuple(inspect.getfullargspec(search_funcs['episodes'])[0][1:])}
+def search_api(table: str):
     args = [request.args.get(param) for param in search_params[table]]
+    return do_search(table, *args)
+
+
+@app.route('/formsearch/<table>', methods=["GET", "POST"])
+def form_search(table: str):
+    """
+    Gets data from HTML form to do a search
+    :param table: database table - contestants or episodes
+    :return: for "GET" request, renders form template. For "POST" request, returns JSON response
+    """
+    if request.method == "POST":
+        args = [request.form.get(param) for param in search_params[table]]
+        return do_search(table, *args)
+    return render_template("form.html", table=table)
+
+
+def do_search(table, *args):
+    """
+    Calls the appropriate database search function, based on the table
+    :param table: database table - contestants or episodes
+    :param args: search parameters to use to supply to the search function
+    :return:
+    """
     search_result = search_funcs[table](*args)
     if search_result:
         return jsonify({table: refactor.make_list(table, search_result)})
